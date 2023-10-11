efb94bf92d95ce671e05c663427994d5dbb3943a
GregBuchie <141267162+GregBuchie@users.noreply.github.com>
2023-10-11 11:50:52 +0100

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,29 +1,8 @@
-# Import libraries
-
 import argparse
 import glob
 import os
-
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
-
-
-# define functions
-def main(args):
-
-mlflow.autolog()
-
-
-    # read data
-    df = get_csvs_df(args.training_data)
-
-    # split data
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
-
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -33,43 +12,26 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
+def split_data(df):
+    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values
+    y = df['Diabetic'].values
+    return X, y
 
-def split_data(X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
-len(X)
-
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
+def train_model(reg_rate, X_train, y_train):
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
 
-
 def parse_args():
-    # setup arg parser
     parser = argparse.ArgumentParser()
-
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
-
-    # parse args
+    parser.add_argument("--training_data", dest='training_data', type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate', type=float, default=0.01)
     args = parser.parse_args()
-
-    # return args
     return args
 
-# run script
+def main(args):
+    df = get_csvs_df(args.training_data)
+    X_train, y_train = split_data(df)
+    train_model(args.reg_rate, X_train, y_train)
+
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
-    # parse args
     args = parse_args()
-
-    # run main function
     main(args)
-
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
